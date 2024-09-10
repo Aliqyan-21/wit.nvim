2c06304913f93596f78417b84a631e60391f87f9
joaomendoncaa <joaomendoncadev@gmail.com>
2024-09-10 14:12:34 +0100

    fix: merge utils to core, since its only 1 function for now

--- lua/wit/core.lua
+++ lua/wit/core.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-local utils = require("wit.utils")
 local config = require("wit.config")
 
 --- @type table<SearchEngine, string>
@@ -13,6 +12,27 @@ M.search_engines = {
 	perplexity = "https://www.perplexity.ai/search?q=",
 }
 
+--- Get the current OS
+--- @return string|nil
+local function get_os()
+    local fh = io.popen("uname")
+
+    if not fh then
+        vim.notify("wit.nvim: uname command not found", vim.log.levels.ERROR)
+        return nil
+    end
+
+    local uname = fh:read("*l")
+
+    fh:close()
+
+    if uname == "Darwin" then
+        return "MacOS"
+    end
+
+    return "Linux"
+end
+
 --- Performs a web search using the configured search engine
 --- @param query string The search query to be executed
 function M.search(query)
@@ -21,10 +41,11 @@ function M.search(query)
 	M.open_url(url)
 end
 
+
 --- Opens a URL in the default browser
 --- @param url string The URL to open
 function M.open_url(url)
-	if utils.get_os() == "Linux" then
+	if get_os() == "Linux" then
 		os.execute("xdg-open '" .. url .. "' > /dev/null 2>&1 &")
 	else
 		os.execute("open '" .. url .. "'")

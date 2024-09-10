7b8b33cc74dea88ec6e2f22718d2b4796e2b066b
joaomendoncaa <joaomendoncadev@gmail.com>
2024-09-10 23:18:21 +0100

    feat: error handling windows
    fix: windows support
    
    fix: windows support
    
    there's a ton of edge cases that need to be covered to support
    windows...

--- lua/wit/core.lua
+++ lua/wit/core.lua
@@ -12,27 +12,6 @@ M.search_engines = {
 	perplexity = "https://www.perplexity.ai/search?q=",
 }
 
---- Get the current OS
---- @return string|nil
-local function get_os()
-    local fh = io.popen("uname")
-
-    if not fh then
-        vim.notify("wit.nvim: uname command not found", vim.log.levels.ERROR)
-        return nil
-    end
-
-    local uname = fh:read("*l")
-
-    fh:close()
-
-    if uname == "Darwin" then
-        return "MacOS"
-    end
-
-    return "Linux"
-end
-
 --- Performs a web search using the configured search engine
 --- @param query string The search query to be executed
 function M.search(query)
@@ -41,16 +20,22 @@ function M.search(query)
 	M.open_url(url)
 end
 
-
 --- Opens a URL in the default browser
 --- @param url string The URL to open
 function M.open_url(url)
-	if get_os() == "Linux" then
-		os.execute("xdg-open '" .. url .. "' > /dev/null 2>&1 &")
+	local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+	local is_macos = vim.fn.has("mac") == 1
+	local is_linux = not is_windows and not is_macos
+
+	if is_windows then
+		vim.notify("Windows is not supported yet", vim.log.levels.ERROR)
+	elseif is_macos then
+		os.execute('open "' .. url .. '"')
+	elseif is_linux then
+		os.execute('xdg-open "' .. url .. '" > /dev/null 2>&1 &')
 	else
-		os.execute("open '" .. url .. "'")
+		vim.notify("Unsupported operating system", vim.log.levels.ERROR)
 	end
 end
 
 return M
-
